ea5bd404127fb48d479b24f437784d2bd0ef7435
phaitoon-sirnil <47589376+phaitoon-sirnil@users.noreply.github.com>
2022-11-23 14:54:14 +0700

    Create KeyboardControlWithImageCapture.py

---
+++ KeyboardControlWithImageCapture.py
@@ -0,0 +1,53 @@
+import KeypressModule as kp
+import cv2
+import time
+
+from djitellopy import tello
+# from time import sleep
+
+kp.init()
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+me.streamon()
+
+def getKeyboardInput():
+    lr, fb, ud, yv = 0,0,0,0
+    speed = 50
+
+    if kp.getKey("LEFT"): lr = -speed
+    elif kp.getKey("RIGHT"): lr = speed
+
+    if kp.getKey("UP"): fb = speed
+    elif kp.getKey("DOWN"): fb = -speed
+
+    if kp.getKey("w"): ud = speed
+    elif kp.getKey("s"): ud = -speed
+
+    if kp.getKey("a"): yv = speed
+    elif kp.getKey("d"): yv = -speed
+
+    if kp.getKey("q"): 
+        yv = 0
+        me.land()
+        
+    if kp.getKey("e"): 
+        yv = 0
+        me.takeoff()
+
+    if kp.getKey("z"):
+        cv2.imwrite(f'resources/images/{time.time()}.jpg',img)
+        time.sleep(0.2)
+        
+    return [lr,fb,ud,yv]
+
+while True:
+    img = me.get_frame_read().frame
+    img = cv2.resize(img,(360,240))
+    cv2.imshow("Image",img)
+
+    vals = getKeyboardInput()
+    me.send_rc_control(vals[0],vals[1],vals[2],vals[3])
+    time.sleep(0.05)
+
